4fbbb7f46d517cdc84c9b2f0c42d6eea9a380f7d
Nicholas Griffin <nicholas.griffin@accrosoft.com>
2019-05-29 01:14:58 +0100

    various  changes

--- sw.js
+++ sw.js
@@ -2,32 +2,60 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/14f20a9935ed8df7f5ec.js",
-    "revision": "5ff4427b20422780b13f788087cb8ffc"
+    "url": "/_nuxt/374c35dcd568f77d8d47.js",
+    "revision": "840ea811b7c4d4c6b0f8ee28fbd05a7a"
   },
   {
-    "url": "/_nuxt/20b7ee2ac65a1a0da292.js",
-    "revision": "a498e0985cb37b5f6edcc086fb4adc6e"
+    "url": "/_nuxt/45ac0f7fc46aa7c3ad9a.js",
+    "revision": "0e90d29c94f874498c3f7d0c3d103972"
   },
   {
-    "url": "/_nuxt/41b1e94fef42d08aed47.js",
-    "revision": "cbd5606a6a72a694469a4c9a58455f24"
+    "url": "/_nuxt/6567371566924b89bd09.js",
+    "revision": "81aefbc8c2fd9e8791c2302b0fe387c0"
   },
   {
-    "url": "/_nuxt/4b312fc1dc422f907bb6.js",
-    "revision": "b48c03b91f6a24e226b78bf06545ae9d"
+    "url": "/_nuxt/6b8a8582a5f82c0f1e06.js",
+    "revision": "ffa7f5a779b60e93d5a04bec105ad933"
   },
   {
-    "url": "/_nuxt/66ac9de6a2c2d5f2a6db.js",
-    "revision": "fb660c72c05a9a3ed741a2ca13c3026c"
+    "url": "/_nuxt/7d74e4f443396720daa5.js",
+    "revision": "5ccfb0cb3649f90a6426dbb6eff62fb6"
   },
   {
-    "url": "/_nuxt/9fa357ce713538a1e592.js",
-    "revision": "435103e4e2b42bd725cb46f4ba98462a"
+    "url": "/_nuxt/8c50d3902d6da8d5999e.js",
+    "revision": "88d0037d88a1ba6933525fc19ad4c704"
   },
   {
-    "url": "/_nuxt/cba0a7e6bd4a1c7d27fe.js",
-    "revision": "483b529cb27331b39fa796ff04b1fb75"
+    "url": "/_nuxt/92e4b7089066e2e02baa.js",
+    "revision": "1f0624391d97a6c270741f79dc4ba412"
+  },
+  {
+    "url": "/_nuxt/a298e545f844038858ba.js",
+    "revision": "d627bab9ada4ed474c1bbbbd58411c55"
+  },
+  {
+    "url": "/_nuxt/c8d4048d477256b0faea.js",
+    "revision": "b62753d526f6ae19006fc7729a9aa1ea"
+  },
+  {
+    "url": "/_nuxt/caba30063dacca87411e.js",
+    "revision": "a21a28c701e7814ec299533a86c4f54d"
+  },
+  {
+    "url": "/_nuxt/d26db56abffc2e10e685.js",
+    "revision": "7bfbf17fd5a87a17bb82741576f53dee"
+  },
+  {
+    "url": "/_nuxt/e8612cbf07d095254218.js",
+    "revision": "3fa62f192f2d8da71ebf9f8c3ea8a802"
+  },
+  {
+    "url": "/_nuxt/ed5ca3c40fa941f82244.js",
+    "revision": "b470a565f5b466a205b5cb7ab2c909b5"
+  },
+  {
+    "url": "/_nuxt/faa1419093dd88d93b3c.js",
+    "revision": "ee9cdf668ac76de4d1d994da3cc0c67d"
   }
 ], {
   "cacheId": "nicholasgriffin",
