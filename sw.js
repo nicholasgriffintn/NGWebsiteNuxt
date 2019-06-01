efac88fa405f532be30c2bfa042a43ffd848890c
Nicholas Griffin <nicholas.griffin@accrosoft.com>
2019-06-01 22:47:42 +0100

    web font loader and  lazy

--- sw.js
+++ sw.js
@@ -10,16 +10,20 @@ workbox.precaching.precacheAndRoute([
     "revision": "8e2bfc4e75eca4201dda630ec7d03587"
   },
   {
+    "url": "/_nuxt/1900e3a4ef7a07b14b0b.js",
+    "revision": "2ed8d0b4858a037161bc4cf566efb7ae"
+  },
+  {
     "url": "/_nuxt/38658e6e14e7ba64db8b.js",
     "revision": "ed192b05c75139c35e1a6fe25cb9bb8d"
   },
   {
-    "url": "/_nuxt/4290f68c1cbf55973ae2.js",
-    "revision": "f8dba7d5e32b11e0524b2e067548a890"
+    "url": "/_nuxt/3d1321baabb631551791.js",
+    "revision": "f5890b57345b0fb8ba6230c995451686"
   },
   {
-    "url": "/_nuxt/45cd76051c06079af8ef.js",
-    "revision": "cf064b2413b772ef203e003cf910403f"
+    "url": "/_nuxt/4290f68c1cbf55973ae2.js",
+    "revision": "f8dba7d5e32b11e0524b2e067548a890"
   },
   {
     "url": "/_nuxt/66fe418448a98683de6c.js",
@@ -34,12 +38,12 @@ workbox.precaching.precacheAndRoute([
     "revision": "da1e904ae32731c7c74a6860b838e13c"
   },
   {
-    "url": "/_nuxt/8b0c2f431e356e51e93c.js",
-    "revision": "311d023ef1ca412c7aaf82efbbef6f55"
+    "url": "/_nuxt/7c676fb365b5f071e4d9.js",
+    "revision": "5b1e4ecacd4bba819db97d4b91fff1d3"
   },
   {
-    "url": "/_nuxt/8b1ef29d53c17104191b.js",
-    "revision": "dc8fe6fe0d0125dd73fcada8d3e4a074"
+    "url": "/_nuxt/8b0c2f431e356e51e93c.js",
+    "revision": "311d023ef1ca412c7aaf82efbbef6f55"
   },
   {
     "url": "/_nuxt/a8b6a36a9709a42c285b.js",
@@ -48,10 +52,6 @@ workbox.precaching.precacheAndRoute([
   {
     "url": "/_nuxt/aecd36c2db2e4855473b.js",
     "revision": "b83d3336188d1cc2d1da5590652eb077"
-  },
-  {
-    "url": "/_nuxt/c2fe9af0c534540cbd4e.js",
-    "revision": "83578ef1b9123837d0883b8d60c6e521"
   },
   {
     "url": "/_nuxt/ca60aa13b40d9b0d286e.js",
